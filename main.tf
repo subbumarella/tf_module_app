df2ca7dbe771d0d2d54033639e09257e6a1a767f
subbumarella <subbumarella1996@gmail.com>
2023-10-04 17:06:16 +0530

    code updated1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "null_resource" "test" {
+resource "null_resource" "ex" {
   provisioner "local-exec" {
-    command="echo Hello world - Env- ${var.env}"
+    command = "echo this is ${var.env} environment"
   }
 }
