9f7637093c73076ecfdd480316e33ae558a4cd7d
subbumarella <subbumarella1996@gmail.com>
2023-10-06 15:26:32 +0530

    updated

--- main.tf
+++ main.tf
@@ -42,6 +42,10 @@ resource "aws_iam_role" "test_role" {
     ]
   })
 }
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.test_role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
 
 resource "aws_security_group" "sg_example"{
     name="${var.component}-${var.env}-sg"
